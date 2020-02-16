e281efd4a924e69792f70ab71a54b0c87f7e5353
John Doe <randomEme@protonmail.com>
2020-02-16 16:55:33 +0400

    Сделал скрипт для показа/скрытия модалок на странице форм

---
+++ app/js/modal.js
@@ -0,0 +1,31 @@
+"use strict"
+
+let modalFail = document.getElementsByClassName("modal")[0];
+let modalSuccess = document.getElementsByClassName("modal")[1];
+let buttonFail = document.getElementsByClassName("modal__button--failure")[0];
+let buttonSuccess = document.getElementsByClassName("modal__button--success")[0];
+
+let submit = document.getElementsByClassName("forms__submit--button")[0];
+
+let inputs = document.getElementsByClassName("forms--important");
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
+  for(let form of inputs) {
+    if(!form.value) {
+      modalFail.classList.add("modal-show");
+      break;
+    }
+  }
+  if(!modalFailure.classList.contains("modal-show")) {
+    modalSuccess.classList.add("modal-show");
+  }
+});
+
+buttonFail.addEventListener("click", (event) => {
+  event.preventDefault();
+  modalFail.classList.remove("modal-show");
+})
+buttonSuccess.addEventListener("click", (event) => {
+  event.preventDefault();
+  modalSuccess.classList.remove("modal-show");
+})
